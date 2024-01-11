4357e38cf308c76e55e2b69ac34c6aa46c8db224
Yan Luka Keita <yanlukakeita2000@gmail.com>
2024-01-11 15:37:54 +0100

    v7

--- __test__/func.test.js
+++ __test__/func.test.js
@@ -3,7 +3,7 @@ import { server } from "../app.js";
 
 const request = supertest(server);
 
-test("Ich erwarte ein Array wenn ich GET benutze", async () => {
+test("Ich erwarte ein Array wenn ich GET benutze", async (done) => {
   const response = await request.get("/");
   expect(response.statusCode).toBe(200);
   expect(response.body).toBeInstanceOf(Object);
